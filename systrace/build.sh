2f8f4cebd086cf9ae14619c5d93bfff6ca67ee0d
weli-l <1289113577@qq.com>
2025-07-07 16:36:10 +0800

    add proto2 version for systrace
    Signed-off-by: weli-l <1289113577@qq.com>

--- systrace/build.sh
+++ systrace/build.sh
@@ -10,11 +10,22 @@ rm src/os/*.skel.h -rf
 
 cp -f config/PyFuncList /etc/systrace/config/PyFuncList
 
+PROTOC_VERSION=$(protoc --version | awk '{print $2}' | cut -d. -f1)
+PROTO_FILE=""
+PROTO_EXTRA_OPT=""
+
+if [ "$PROTOC_VERSION" -ge 3 ]; then
+    PROTO_FILE="systrace.v3.proto"
+else
+    PROTO_FILE="systrace.v2.proto"
+fi
+
 cd protos
-protoc --c_out=. systrace.proto
-protoc --cpp_out=. systrace.proto
-protoc --python_out=. systrace.proto
+protoc --c_out=. $PROTO_FILE
+protoc --cpp_out=. $PROTO_FILE
+protoc --python_out=. $PROTO_FILE
 cd ..
+
 cd build
 cmake ..
 make -j $(nproc)
