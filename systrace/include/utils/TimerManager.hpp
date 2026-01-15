162ada77454d98553fbe61a2159b788284910e9d
weli1 <1289113577@qq.com>
2026-01-15 19:09:13 +0800

    fix timer cannot support multi plugin
    Signed-off-by: weli1 <1289113577@qq.com>

--- systrace/include/utils/TimerManager.hpp
+++ systrace/include/utils/TimerManager.hpp
@@ -6,13 +6,20 @@
 #include <condition_variable>
 #include <map>
 #include <atomic>
-#include <vector>
+#include <memory>
+#include <string>
 
 namespace systrace {
 namespace utils {
 
 class TimerManager {
 public:
+    struct TimerTask {
+        std::atomic<bool> active{true};
+        std::mutex task_mtx;
+        std::condition_variable task_cv;
+    };
+
     static TimerManager& getInstance() {
         static TimerManager instance;
         return instance;
@@ -21,58 +28,68 @@ public:
     void startTimer(const std::string& id, int duration, std::function<void()> callback) {
         stopTimer(id);
 
-        std::lock_guard<std::mutex> lock(mtx_);
-        auto is_active = std::make_shared<std::atomic<bool>>(true);
-        active_tasks_[id] = is_active;
+        auto task = std::make_shared<TimerTask>();
+        {
+            std::lock_guard<std::mutex> lock(map_mtx_);
+            active_tasks_[id] = task;
+        }
 
-        std::thread([this, id, duration, callback, is_active]() {
+        std::thread([this, id, duration, callback, task]() {
             {
-                std::unique_lock<std::mutex> lk(this->cv_mtx_);
-                this->cv_.wait_for(lk, std::chrono::seconds(duration), [is_active] {
-                    return !is_active->load();
+                std::unique_lock<std::mutex> lk(task->task_mtx);
+                task->task_cv.wait_for(lk, std::chrono::seconds(duration), [&task] {
+                    return !task->active.load();
                 });
             }
 
-            if (is_active->load()) {
+            if (task->active.load()) {
                 callback();
             }
 
-            std::lock_guard<std::mutex> clean_lock(this->mtx_);
+            std::lock_guard<std::mutex> lock(this->map_mtx_);
             auto it = this->active_tasks_.find(id);
-            if (it != this->active_tasks_.end() && it->second == is_active) {
+            if (it != this->active_tasks_.end() && it->second == task) {
                 this->active_tasks_.erase(it);
             }
         }).detach();
     }
 
     void stopTimer(const std::string& id) {
-        std::lock_guard<std::mutex> lock(mtx_);
-        auto it = active_tasks_.find(id);
-        if (it != active_tasks_.end()) {
-            it->second->store(false);
-            active_tasks_.erase(it);
-            cv_.notify_all();
+        std::shared_ptr<TimerTask> task_to_stop;
+        {
+            std::lock_guard<std::mutex> lock(map_mtx_);
+            auto it = active_tasks_.find(id);
+            if (it != active_tasks_.end()) {
+                task_to_stop = it->second;
+                active_tasks_.erase(it);
+            }
+        }
+
+        if (task_to_stop) {
+            task_to_stop->active.store(false);
+            task_to_stop->task_cv.notify_one();
         }
     }
 
     void clearAll() {
-        std::lock_guard<std::mutex> lock(mtx_);
+        std::lock_guard<std::mutex> lock(map_mtx_);
         for (auto& pair : active_tasks_) {
-            pair.second->store(false);
+            pair.second->active.store(false);
+            pair.second->task_cv.notify_one();
         }
         active_tasks_.clear();
-        cv_.notify_all();
     }
 
 private:
     TimerManager() = default;
     ~TimerManager() { clearAll(); }
     
-    std::mutex mtx_;
-    std::mutex cv_mtx_;
-    std::condition_variable cv_;
-    std::map<std::string, std::shared_ptr<std::atomic<bool>>> active_tasks_;
+    std::mutex map_mtx_;
+    std::map<std::string, std::shared_ptr<TimerTask>> active_tasks_;
+
+    TimerManager(const TimerManager&) = delete;
+    TimerManager& operator=(const TimerManager&) = delete;
 };
 
-} // namespace utils
-} // namespace systrace
+} 
+}
